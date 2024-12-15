8a831703ca979b7382c32d75c76704ffb2394fdd
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2024-12-15 14:05:04 +0100

    Add project files.

---
+++ WpfBlazorHybridApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfBlazorHybridApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var services= new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+            this.Resources.Add("ServiceProvider", serviceProvider);
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddWpfBlazorWebView();
+
+    #if DEBUG
+            services.AddBlazorWebViewDeveloperTools();
+    #endif
+        }
+    }
+
+}
